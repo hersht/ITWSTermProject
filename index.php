7bf01db79ae553df6f978dbb1f737caa9297b93a
rvm2113 <rutvikmanohar@gmail.com>
2017-11-28 22:48:32 -0500

    Fetched room data

--- index.php
+++ index.php
@@ -85,7 +85,7 @@
   
   /* Create a new database connection object, passing in the host, username,
      password, and database to use. The "@" suppresses errors. */
-  @ $db = new mysqli('localhost', 'root', 'mypass', 'iit');
+  @ $db = new mysqli('localhost', 'root', 'cestlafin1', 'iit');
   
   if ($db->connect_error) {
     echo '<div class="messages">Could not connect to the database. Error: ';
